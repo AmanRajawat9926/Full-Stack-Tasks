8c777905666119aa2e5fbf321375af021bd0f328
Aman Rajawat <rajawat7241@gmail.com>
2025-08-01 10:15:12 -0700

    Day 10

---
+++ Task10.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("button");
+let div = document.querySelector("div");
+let heading = document.querySelector("h2");
+btn.addEventListener("click", getRandomColor);
+
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);        
+    let blue = Math.floor(Math.random() * 256);
+    div.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    heading.innerHTML = `RGB(${red}, ${green}, ${blue})`;
+}
